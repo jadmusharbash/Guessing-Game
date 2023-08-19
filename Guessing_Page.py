c0ae4cecf6ac2b6aa2e3fcdc9234b1be1d224430
jadmusharbash <jmusharbash.25@acsamman.edu.jo>
2023-08-19 18:06:35 +0300

    added number range, also another if statement

--- Guessing_Page.py
+++ Guessing_Page.py
@@ -11,3 +11,7 @@ def guess_number(target): # If statements (is it Increase or Decrease)
             print("Congrats! You found the number.")
             break
 
+if __name__ == "__main__":
+    target_number = random.randint(1, 50)  # Number range
+    print("The Number Guessing Game!")
+    guess_number(target_number)
